4c4ba2cde5ab7709d414d13920c2638e70167a06
Ninylka <shemetylka@gmail.com>
2023-10-16 20:54:16 +0200

    supportnew2

--- JS/first.js
+++ JS/first.js
@@ -221,114 +221,196 @@
 // Практика
 // Потрібно створити два приклади годинника (Електронний та механічний)
 
-const arrDay = ['Неділя', 'Понеділок', 'Вівторок', 'Середа', 'Четвер', 'П`ятниця', 'Субота'];
+// const arrDay = ['Неділя', 'Понеділок', 'Вівторок', 'Середа', 'Четвер', 'П`ятниця', 'Субота'];
 
-const namesOfMonth = ['Січень', 'Лютий', 'Березень', 'Квітень', 'Травень', 'Червень', 'Липень', 'Серпень', 'Вересень', 'Жовтень', 'Листопад', 'Грудень'];
+// const namesOfMonth = ['Січень', 'Лютий', 'Березень', 'Квітень', 'Травень', 'Червень', 'Липень', 'Серпень', 'Вересень', 'Жовтень', 'Листопад', 'Грудень'];
 
-const selectors = {
-    day: document.querySelector('.date-day'),
-    date: document.querySelector('.date'),
-    month: document.querySelector('.date-month'),
-    year: document.querySelector('.date-year'),
+// const selectors = {
+//     day: document.querySelector('.date-day'),
+//     date: document.querySelector('.date'),
+//     month: document.querySelector('.date-month'),
+//     year: document.querySelector('.date-year'),
 
-    clock: document.querySelector('.digital-clock'),
+//     clock: document.querySelector('.digital-clock'),
 
-    seconds: document.querySelector('.clock-seconds__arrow'),
-    minutes: document.querySelector('.clock-minutes__arrow'),
-    hours: document.querySelector('.clock-hours__arrow')
-};
+//     seconds: document.querySelector('.clock-seconds__arrow'),
+//     minutes: document.querySelector('.clock-minutes__arrow'),
+//     hours: document.querySelector('.clock-hours__arrow')
+// };
 
 // selectors.second.style.transform = 'rotate(120deg)';
 
-const timeCb = () => {
-    console.log('SetInterval')
-    const currentDate = new Date();
+// const timeCb = () => {
+//     console.log('SetInterval')
+//     const currentDate = new Date();
 
-    const day = currentDate.getDay();
-    const date = currentDate.getDate();
-    const month = currentDate.getMonth();
-    const year = currentDate.getFullYear();
+//     const day = currentDate.getDay();
+//     const date = currentDate.getDate();
+//     const month = currentDate.getMonth();
+//     const year = currentDate.getFullYear();
 
-    const localTime = currentDate.toLocaleTimeString('uk-UA');
-    // console.log(localTime);
+//     const localTime = currentDate.toLocaleTimeString('uk-UA');
+//     // console.log(localTime);
 
-    // console.log(day, date, month, year)
+//     // console.log(day, date, month, year)
 
-    selectors.day.textContent = arrDay[day];
-    selectors.month.textContent = namesOfMonth[month];
-    selectors.date.textContent = date;
-    selectors.year.textContent = year;
-    selectors.clock.textContent = localTime;
+//     selectors.day.textContent = arrDay[day];
+//     selectors.month.textContent = namesOfMonth[month];
+//     selectors.date.textContent = date;
+//     selectors.year.textContent = year;
+//     selectors.clock.textContent = localTime;
 
-    const hours = currentDate.getHours();
-    const minutes = currentDate.getMinutes();
-    const seconds = currentDate.getSeconds();
-    // console.log('Seconds: ' + seconds);
+//     const hours = currentDate.getHours();
+//     const minutes = currentDate.getMinutes();
+//     const seconds = currentDate.getSeconds();
+//     // console.log('Seconds: ' + seconds);
 
-    const secondsDeg = 360 / 60 * seconds;
-    const minutesDeg = 360 / 60 * minutes;
-    const hoursDeg = 360 / 12 * hours + (360 / 12 / 60) * minutes;
-    // console.log('SecondsDeg: ' + secondsDeg)
+//     const secondsDeg = 360 / 60 * seconds;
+//     const minutesDeg = 360 / 60 * minutes;
+//     const hoursDeg = 360 / 12 * hours + (360 / 12 / 60) * minutes;
+//     // console.log('SecondsDeg: ' + secondsDeg)
 
-    selectors.seconds.style.transform = `rotate(${secondsDeg}deg)`;
-    selectors.minutes.style.transform = `rotate(${minutesDeg}deg)`;
-    selectors.hours.style.transform = `rotate(${hoursDeg}deg)`;
+//     selectors.seconds.style.transform = `rotate(${secondsDeg}deg)`;
+//     selectors.minutes.style.transform = `rotate(${minutesDeg}deg)`;
+//     selectors.hours.style.transform = `rotate(${hoursDeg}deg)`;
 
-}
-
-timeCb();
-
-setInterval(timeCb, 1000)
-
-
-
-
-import throttle from "lodash.throttle";
-
-const formEl = document.querySelector('.feedback-form');
-
-formEl.addEventListener("input", throttle(onInputData, 500));
-formEl.addEventListener("submit", onSubmit);
-
-const input = formEl.email;
-const texteria = formEl.message;
-const STORAGE_KEY = 'feedback-form-state';
-let formElements = {};
-dataLocalStorage();
-
-function onInputData(event) {
-// const userData = {
-//   email: input.value.trim(),
-//   message: texteria.value.trim(),
 // }
 
-formElements[event.target.name] = event.target.value
-localStorage.setItem('STORAGE_KEY', JSON.stringify(formElements));
+// timeCb();
 
-};
+// setInterval(timeCb, 1000)
 
-function onSubmit(event) {
-  event.preventDefault();
-  const { email, message } = event.currentTarget.elements;
- const dataForm = {
-        email: email.value,
-        message: message.value,
-    };
-  if (email.value === '' || message.value === '') {
-   alert(`Please fill in all the fields!`);
- return formEl;
-  }
+
+
+
+// import throttle from "lodash.throttle";
+
+// const formEl = document.querySelector('.feedback-form');
+
+// formEl.addEventListener("input", throttle(onInputData, 500));
+// formEl.addEventListener("submit", onSubmit);
+
+// const input = formEl.email;
+// const texteria = formEl.message;
+// const STORAGE_KEY = 'feedback-form-state';
+// let formElements = {};
+// dataLocalStorage();
+
+// function onInputData(event) {
+// // const userData = {
+// //   email: input.value.trim(),
+// //   message: texteria.value.trim(),
+// // }
+
+// formElements[event.target.name] = event.target.value
+// localStorage.setItem('STORAGE_KEY', JSON.stringify(formElements));
+
+// };
+
+// function onSubmit(event) {
+//   event.preventDefault();
+//   const { email, message } = event.currentTarget.elements;
+//  const dataForm = {
+//         email: email.value,
+//         message: message.value,
+//     };
+//   if (email.value === '' || message.value === '') {
+//    alert(`Please fill in all the fields!`);
+//  return formEl;
+//   }
   
-  console.log(dataForm);
-  localStorage.removeItem('STORAGE_KEY');
-   formEl.reset();
+//   console.log(dataForm);
+//   localStorage.removeItem('STORAGE_KEY');
+//    formEl.reset();
 
+// }
+
+
+// function dataLocalStorage() {
+//   if (JSON.parse(localStorage.getItem('STORAGE_KEY'))) {
+//     input.value = JSON.parse(localStorage.getItem('STORAGE_KEY')).email;
+//     texteria.value = JSON.parse(localStorage.getItem('STORAGE_KEY')).message;
+//   }
+// }
+const delay = (ms) => {
+  return new Promise((resolve) => {
+    setTimeout(() => { 
+    resolve (ms)
+  }, ms)
+    
+  })
 }
+const logger = time => console.log(`resolved after ${time}ms`)
+delay(2000).then(a => logger(a));
+delay(1000).then(b => logger(b));
 
 
-function dataLocalStorage() {
-  if (JSON.parse(localStorage.getItem('STORAGE_KEY'))) {
-    input.value = JSON.parse(localStorage.getItem('STORAGE_KEY')).email;
-    texteria.value = JSON.parse(localStorage.getItem('STORAGE_KEY')).message;
-  }
-}
+
+
+// const supportPage = [
+//     {
+//         title: 'Save the Children',
+//         url:
+//           'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
+        
+//         img: './img/support/save_children.png" alt="save_children" width="131"',
+//         img2x: './img/support/save_children@2x.png" alt="save_children" width="131"',
+//         p: '01',
+//       },
+//       {
+//         title: 'Project HOPE',
+//         url: 'https://www.projecthope.org/country/ukraine/',
+//         img: './img/support/project_hope.png" alt="project_hope" width="62"',
+//         img2x: './img/support/project_hope@2x.png" alt="project_hope" width="62"',
+//         p: '02',
+//       },
+//       {
+//         title: 'International Medical Corps',
+//         url: 'https://internationalmedicalcorps.org/country/ukraine/',
+//         img: './img/support/medical_corps.png" alt="medical_corps" width="101"',
+//         img2x: './img/support/medical_corps@2x.png" alt="medical_corps" width="101"',
+//         p: '03',
+//       },
+//       {
+//         title: 'RAZOM',
+//         url: 'https://www.razomforukraine.org/',
+//         img: './img/support/razom.png" alt="razom" width="82"',
+//         img2x: './img/support/razom@2x.png" alt="razom" width="82"',
+//         p: '04',
+//       },
+//       {
+//         title: 'Action against hunger',
+//         url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
+//         img: './img/support/action_against_hunger.png" alt="action_against_hunger" width="55"',
+//         img2x: './img/support/action_against_hunger@2x.png" alt="action_against_hunger" width="55"',
+//         p: '05',
+//       },
+//       {
+//         title: 'Serhiy Prytula Charity Foundation',
+//         url: 'https://prytulafoundation.org/en',
+//         img: './img/support/sergiy_prytyla.png" alt="sergiy_prytyla" width="115"',
+//         img2x: './img/support/sergiy_prytyla@2x.png" alt="sergiy_prytyla" width="115"',
+//         p: '06',
+//       },
+//       {
+//         title: 'Medicins Sans Frontieres',
+//         url: 'https://www.msf.org/ukraine',
+//         img: './img/support/medicins_sans.png" alt="medicins_sans" width="102"',
+//         img2x: './img/support/medicins_sans@2x.png" alt="medicins_sans" width="102"',
+//         p: '07',
+//       },
+//       {
+//         title: 'World vision',
+//         url: 'https://www.wvi.org/emergencies/ukraine',
+//         img: './img/support/world_vision.png" alt="world_vision" width="81"',
+//         img2x: './img/support/world_vision@2x.png" alt="world_vision" width="81"',
+//         p: '08',
+//       },
+//       {
+//         title: 'UNITED24',
+//         url: 'https://u24.gov.ua/uk',
+//         img: './img/support/united24.png" alt="united24" width="114"',
+//         img2x: './img/support/united24@2x.png" alt="united24" width="114"',
+//         p: '09',
+//       },
+//   ];
